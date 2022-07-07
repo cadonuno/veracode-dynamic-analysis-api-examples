b8bb45279c462d55b3b7262516128deb650b7903
Ricardo Alvares Pereira <rpereira@veracode.com>
2022-07-07 12:49:17 +0100

    Adding support for creating app profiles and selecting an option when multiple profiles are found

--- veracode-da-app-link.py
+++ veracode-da-app-link.py
@@ -18,8 +18,40 @@ def print_help():
 """)
     sys.exit()
 
+def create_application(application_name, criticality, verbose):
+    if criticality is None:
+        print("To create an application, it is necessary to provide a business criticality (--criticality, -c)")
+        return False
+    print(f"Creating application called: {application_name}")
+    data = application_name
+    scan_request = r'''{
+    "profile":{
+        "name":"{application_name}",
+        "business_criticality":"{criticality}"
+    }
+}'''
+    path = "https://api.veracode.com/appsec/v1/applications"
+    scan_request = scan_request.replace("{application_name}", application_name, 1)
+    scan_request = scan_request.replace("{criticality}", criticality, 2)
 
-def launch_app_linked_scan(application_name, url, verbose):
+    if verbose:
+        print(scan_request)
+
+    response = requests.post(path, auth=RequestsAuthPluginVeracodeHMAC(), headers=headers, json=json.loads(scan_request))
+
+    if verbose:
+        print(f"status code {response.status_code}")
+        body = response.json()
+        if body:
+            print(body)
+    if response.status_code == 200:
+        print("Successfully created application profile.")
+        return True
+    else:
+        print(f"Unable to create application profile: {response.status_code}")
+        return False
+
+def launch_app_linked_scan(application_name, url, verbose, criticality, start_now):
     """Lists the scanner variables that are defined at the organization level. """
     path = api_base + f"platform_applications?application_name={application_name}"
     response = requests.get(path, auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
@@ -28,17 +60,39 @@ def launch_app_linked_scan(application_name, url, verbose):
     if verbose:
         print(data)
 
+    print(f"Initializing scan for url: {url} and search query {application_name}")
+
     if len(data["_embedded"]["platform_applications"]) == 0:
-        print("No applications defined.")
+        print("No applications defined, creating new application.")
+        if create_application(application_name, criticality, verbose):
+            launch_app_linked_scan(application_name, url, verbose, criticality, start_now)
         return
-    if len(data["_embedded"]["platform_applications"]) != 1:
-        print("Warning - too many matching applications, the first will be used.")
-    uuid = data["_embedded"]["platform_applications"][0]["uuid"]
-    matched_app_name = data["_embedded"]["platform_applications"][0]["name"]
-    print(f"Found uuid '{uuid}' for application '{matched_app_name}' for search term '{application_name}'")
+    
+    if len(data["_embedded"]["platform_applications"]) > 1:
+        print("Warning - multiple applications found for {application_name}")
+        print("Please select one of the following applications: ")
+        for index in range(len(data["_embedded"]["platform_applications"])):
+            app_name = data["_embedded"]["platform_applications"][index]["name"]
+            app_uuid = data["_embedded"]["platform_applications"][index]["uuid"]
+            print(f" {index+1} - {app_uuid}: {app_name}")
+        selectedIndexStr = input("Insert application index:")
+        if selectedIndexStr and selectedIndexStr.isnumeric():
+            selectedIndex = int(selectedIndexStr)-1
+        print(selectedIndex)
+        if selectedIndex < 0 or selectedIndex > len(data["_embedded"]["platform_applications"]):
+            print("Invalid application selected, closing application")
+            return
+    else:
+        print("Application found")
+        selectedIndex = 0        
+
+    matched_app_name = data["_embedded"]["platform_applications"][selectedIndex]["name"]
+    uuid = data["_embedded"]["platform_applications"][selectedIndex]["uuid"]
+    
+    print(f"Starting Dynamic Analysis for uuid '{uuid}' and application name '{matched_app_name}' for search term '{application_name}'")
                                            
     scan_request = r'''{
-          "name": "Example Veracode applinked scan launched from API",
+          "name": "{scan_name}",
           "scans": [
             {
               "linked_platform_app_uuid": "{uuid}",
@@ -50,17 +104,22 @@ def launch_app_linked_scan(application_name, url, verbose):
                 }
               }
             }
-          ],
-          "schedule": {
+          ]{schedule}        
+        }'''
+    scan_request = scan_request.replace("{uuid}", uuid, 1)
+    scan_request = scan_request.replace("{url}", url, 2)
+    scan_request = scan_request.replace("{scan_name}", url, 2)
+    if start_now:
+        schedule=r''', "schedule": {
             "now": true,
             "duration": {
               "length": 1,
               "unit": "DAY"
             }
-          }
-        }'''
-    scan_request = scan_request.replace("{uuid}", uuid, 1)
-    scan_request = scan_request.replace("{url}", url, 2)
+          }'''
+    else:
+        schedule = ""
+    scan_request = scan_request.replace("{schedule}", schedule)    
 
     if verbose:
         print(scan_request)
@@ -74,21 +133,31 @@ def launch_app_linked_scan(application_name, url, verbose):
         if body:
             print(body)
     if response.status_code == 201:
-        print("Successfully started applinked scan.")
+        print("Successfully created applinked scan.")
+        if start_now:
+            print("Successfully started scanning the application")
+    else:
+        print(f"Unable to create DAST scan: {response.status_code}")
 
 def main(argv):
     """Simple command line support for creating, deleting, and listing DA scanner variables"""
     try:
         application_name = ''
         verbose = False
+        start_now = False
+        criticality=None
 
-        opts, args = getopt.getopt(argv, "hda:u:",
-                                   ["application_name=", "target_url="])
+        opts, args = getopt.getopt(argv, "hda:u:c:n",
+                                   ["application_name=", "target_url=", "criticality="])
         for opt, arg in opts:
             if opt == '-h':
                 print_help()
             if opt == '-d':
                 verbose = True
+            if opt == '-n':
+                start_now = True
+            if opt in ('-c', '--criticality'):
+                criticality=arg
             if opt in ('-a', '--application_name'):
                 application_name = arg
             if opt in ('-u', '--url'):
@@ -96,7 +165,7 @@ def main(argv):
 
 
         if application_name and target_url:
-            launch_app_linked_scan(application_name, target_url, verbose)
+            launch_app_linked_scan(application_name, target_url, verbose, criticality, start_now)
         else:
             print_help()
 
